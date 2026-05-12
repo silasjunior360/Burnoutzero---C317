0a0d516c047da51c9dbf6a194cfc0ab3b1b96157
silasjunior360 <silas36junior@gmail.com>
2026-05-12 02:46:52 -0300

    fix: remove unused import of React in LandingPage component

--- frontend/pages/LandingPage.tsx
+++ frontend/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { theme } from '../theme';
 
@@ -439,4 +439,5 @@ const BurnoutZeroLanding = () => {
 };
 
 export default BurnoutZeroLanding;
+
 
