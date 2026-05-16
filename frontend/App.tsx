ecc8b1e9426eb25b31aab957b76ead6b857a551c
silasjunior360 <silas36junior@gmail.com>
2026-05-16 04:13:32 -0300

    fix: add missing closing bracket in Box component

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -23,7 +23,7 @@ function AppShell() {
     <>
         <Header />
 
-        <Box component="main" sx={{ minHeight: 'calc(100vh - 140px)', p: 0, bgcolor: theme.palette.background.default }
+        <Box component="main" sx={{ minHeight: 'calc(100vh - 140px)', p: 0, bgcolor: theme.palette.background.default }}>
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/home" element={<Home />} />
